30d71861d397b643eb4c495b424c30434b2d228d
Connor Maddox <connor@connstruct.tech>
2025-05-13 00:28:02 -0500

    Adjust scrollbar behavior.

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import spectre from './package/src';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import { spectreDark } from './src/ec-theme';
 
 // https://astro.build/config
@@ -46,7 +47,8 @@ export default defineConfig({
       }
     })
   ],
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify()
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
 });
